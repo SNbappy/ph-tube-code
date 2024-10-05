0b8e8c01d0b3ef06b0a6885020eeeda0298f9aed
SNbappy <bappy.just@gmail.com>
2024-10-05 08:04:07 +0600

    thumbnail added dynamically

--- scripts/video.js
+++ scripts/video.js
@@ -10,10 +10,44 @@ const loadCategories = () => {
         .catch((error) => console.log(error));
 }
 
+const loadVideos = () => {
+    // fetch the data
+    fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+        .then((res) => res.json())
+        .then((data) => displayVideos(data.videos))
+        .catch((error) => console.log(error));
+}
+
+const displayVideos = (videos) => {
+    const videoContainer = document.getElementById("videos");
+
+    videos.forEach((video) => {
+        console.log(video);
+        const card = document.createElement("div");
+        card.classList = "card card-compact";
+        card.innerHTML =
+            `
+            <figure>
+    <img
+      src=${video.thumbnail};
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+            `;
+        videoContainer.append(card);
+    });
+}
+
 // create displayCategories
 const displayCategories = (categories) => {
     const categoryContainer = document.getElementById("categories");
-        
+
     categories.forEach((item) => {
         console.log(item);
         // create a button
@@ -28,3 +62,4 @@ const displayCategories = (categories) => {
 }
 
 loadCategories();
+loadVideos();
